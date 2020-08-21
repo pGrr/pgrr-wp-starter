7c28a5b7c7f4f2a9fab53d86cb126feea7af9bd4
Paolo Garroni <garroni.paolo@gmail.com>
2020-08-21 22:15:40 +0200

    add bootstrap-navwalker

--- header.php
+++ header.php
@@ -60,8 +60,8 @@
 						'depth'             => 2,
 						'container'         => 'div',
 						'container_class'   => 'collapse navbar-collapse',
-						'container_id'      => 'bs-example-navbar-collapse-1',
-						'menu_class'        => 'nav navbar-nav',
+						'container_id'      => 'primary-menu',
+						'menu_class'        => 'nav navbar-nav ml-auto',
 						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
 						'walker'            => new WP_Bootstrap_Navwalker(),
 					));
