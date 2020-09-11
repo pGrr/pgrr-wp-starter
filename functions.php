c2f4942147d781af7424a9eb1caa7e951f7918c3
Paolo Garroni <garroni.paolo@gmail.com>
2020-09-11 19:29:18 +0200

    add scrollreveal

--- functions.php
+++ functions.php
@@ -143,8 +143,9 @@ function pgrr_scripts() {
 	wp_enqueue_style( 'pgrr-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'pgrr-style', 'rtl', 'replace' );
 	wp_enqueue_script( 'wp-api' );
-	wp_enqueue_script('fontawesome', 'https://kit.fontawesome.com/46de57cbc9.js', array(), _S_VERSION, true);
-	wp_enqueue_script( 'pgrr-script', get_template_directory_uri() . '/script.js', array(), _S_VERSION, true );
+	wp_enqueue_script('fontawesome', 'https://kit.fontawesome.com/46de57cbc9.js', array(), _S_VERSION, false);
+    wp_enqueue_script('scrollreveal', 'https://unpkg.com/scrollreveal@4.0.0/dist/scrollreveal.min.js', array(), _S_VERSION, false);
+    wp_enqueue_script( 'pgrr-script', get_template_directory_uri() . '/script.js', array(), _S_VERSION, true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
